77ff6fe34a329216c6bbd66646d57f089927d3ca
takaiyuk <takaishikawa1225@gmail.com>
2022-07-06 23:02:10 +0900

    Use dotenvy

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use anyhow::Result;
+use dotenvy::dotenv;
 use std::env;
 
 use kakeibo_rs::ifttt::IFTTTAPIParams;
@@ -8,15 +9,22 @@ use kakeibo_rs::slack::SlackAPIParams;
 
 #[cfg(not(tarpaulin_include))]
 fn main() -> Result<()> {
+    dotenv().ok();
+
     let slack_channel_id = env::var("SLACK_CHANNEL_ID").expect("$SLACK_CHANNEL_ID is not set");
     let slack_token = env::var("SLACK_TOKEN").expect("$SLACK_TOKEN is not set");
-    let slack_messages = SlackAPI::new(SlackAPIParams::new(slack_channel_id, slack_token)).extract();
-    for m in &slack_messages {
-        println!("message to be posted: {},{}", m.timestamp, m.text)
-    }
+    let slack_messages =
+        SlackAPI::new(SlackAPIParams::new(slack_channel_id, slack_token)).extract();
+    slack_messages.iter().for_each(|m| {
+        println!("{},{}", m.timestamp, m.text);
+    });
 
-    let ifttt_event_name = env::var("IFTTT_EVENT_NAME").expect("$IFTTT_EVENT_NAME is not set");
-    let ifttt_webhook_token = env::var("IFTTT_WEBHOOK_TOKEN").expect("$IFTTT_WEBHOOK_TOKEN is not set");
-    IFTTTAPI::new(IFTTTAPIParams::new(ifttt_event_name, ifttt_webhook_token)).kick(slack_messages);
+    if !slack_messages.is_empty() {
+        let ifttt_event_name = env::var("IFTTT_EVENT_NAME").expect("$IFTTT_EVENT_NAME is not set");
+        let ifttt_webhook_token =
+            env::var("IFTTT_WEBHOOK_TOKEN").expect("$IFTTT_WEBHOOK_TOKEN is not set");
+        IFTTTAPI::new(IFTTTAPIParams::new(ifttt_event_name, ifttt_webhook_token))
+            .kick(slack_messages);
+    }
     Ok(())
 }
